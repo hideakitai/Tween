a67f017ad1cfab208033b7f82af9b6b1c940772c
Hideaki Tai <hideaki.tai@gmail.com>
2020-09-29 18:08:15 +0900

    update namespace and compile flag based on new arx

--- Tween/Types.h
+++ Tween/Types.h
@@ -2,24 +2,11 @@
 #ifndef HT_TWEEN_TYPES_H
 #define HT_TWEEN_TYPES_H
 
-#if defined(ARDUINO_ARCH_AVR)\
- || defined(ARDUINO_ARCH_MEGAAVR)\
- || defined(ARDUINO_ARCH_SAMD)\
- || defined(ARDUINO_spresense_ast)
-    #define HT_TWEEN_STL_DISABLED
-#endif
-
 #include <Arduino.h>
-#ifdef HT_TWEEN_STL_DISABLED
-    #include "Tween/util/TeensyDirtySTLErrorSolution/TeensyDirtySTLErrorSolution.h"
-    #include "Tween/util/ArxTypeTraits/ArxTypeTraits.h"
-    #include "Tween/util/ArxContainer/ArxContainer.h"
-    #include "Tween/util/ArxSmartPtr/ArxSmartPtr.h"
-#else
-    #include <vector>
-    #include <map>
-    #include <memory>
-#endif
+#include "Tween/util/TeensyDirtySTLErrorSolution/TeensyDirtySTLErrorSolution.h"
+#include "Tween/util/ArxTypeTraits/ArxTypeTraits.h"
+#include "Tween/util/ArxSmartPtr/ArxSmartPtr.h"
+#include "Tween/util/ArxContainer/ArxContainer.h"
 
 namespace ht {
 namespace tween {
@@ -37,17 +24,17 @@ namespace tween {
         uint32_t duration {0};
     };
 
-#ifdef HT_TWEEN_STL_DISABLED
-    template <typename T, typename U> using Map = arx::map<T, U>;
-    template <typename T> using Vec = arx::vector<T>;
-    template <typename T> using Ref = arx::shared_ptr<T>;
-    using namespace arx;
-#else
+#if ARX_HAVE_LIBSTDCPLUSPLUS >= 201103L // Have libstdc++11
     template <typename T, typename U> using Map = std::map<T, U>;
     template <typename T> using Vec = std::vector<T>;
     template <typename T> using Ref = std::shared_ptr<T>;
     using namespace std;
-#endif // HT_TWEEN_STL_DISABLED
+#else
+    template <typename T, typename U> using Map = arx::map<T, U>;
+    template <typename T> using Vec = arx::vector<T>;
+    template <typename T> using Ref = arx::shared_ptr<T>;
+    using namespace arx;
+#endif
 
 } // tween
 } // ht
